23ecc7e0422dfc5c786ec9fd0f7fecf9c83c8b8c
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-20 20:24:06 -0300

    data type numbers

---
+++ 01_basico/02_datatypes.dart
@@ -0,0 +1,15 @@
+main() {
+  // Numeros
+  // var a = 10;
+  // int b = 10;
+  // double c = 10.3;
+
+  int a = 10;
+  double b = 5.5;
+  int? c; // ? significa que puede ser nulo
+
+  int _a = 30;
+  double $b = 40;
+
+  print(_a + $b);
+}
