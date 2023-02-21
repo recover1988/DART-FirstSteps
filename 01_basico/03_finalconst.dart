ee95d2db0dc1f4242d9f4def061f97d8a0f08aea
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-20 22:46:04 -0300

    final

---
+++ 01_basico/03_finalconst.dart
@@ -0,0 +1,15 @@
+main() {
+  var a = 10;
+  final double b = 10.0;
+  const double c = 10;
+
+  final List<String> personasFinal = ['Juan', 'Pedro', 'Fernando'];
+  const List<String> personasConst = ['Juan', 'Pedro', 'Fernando'];
+  List<String> personasConst2 = const ['Juan', 'Pedro', 'Fernando'];
+
+  personasFinal.add('Maria');
+  // personasConst.add('Maria');
+
+  // print(personasFinal);
+  // print(personasConst);
+}
