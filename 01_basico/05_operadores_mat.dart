fcbeaeab8bfc343581f20d9571b7896dff02efc5
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-21 01:14:38 -0300

    operadores matematicos

---
+++ 01_basico/05_operadores_mat.dart
@@ -0,0 +1,31 @@
+/**
+ * Un operador es un simbolo que le dice al compilador
+ * que debe realizar una tarea
+ * matematica, relacional o logica
+ * y debe producir un resultad
+ * 
+ */
+
+main() {
+  int a = 10 + 5; // + 15
+
+  a = 20 - 10; // - 10
+
+  a = 10 * 2; // * 20
+
+  double b = 10 / 2; //  / 5
+
+  b = 10.0 % 3; // %  1
+
+  b = -b; // -expr  -> es usado para cambiar el valor de la exprecion
+
+  int c = 10 ~/ 3; // ~/ 3 --> parte entera de la division
+
+  int d = 1;
+
+  d++; // ++ 2
+  d--; // -- 1
+
+  d += 2; // += 3
+  d -= 2; // -= 1   *=  /=
+}
