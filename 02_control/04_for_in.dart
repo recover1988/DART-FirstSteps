0ba0b6087aaeaa55ace734469c32a527c045ec70
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-21 23:08:30 -0300

    for in

---
+++ 02_control/04_for_in.dart
@@ -0,0 +1,10 @@
+main() {
+  List<String> listado = ['Batman', 'Superman', 'Wonderwoman'];
+  // for (int i = 0; i < listado.length; i++) {
+  //   print(listado[i]);
+  // }
+
+  for (String heroe in listado) {
+    print(heroe);
+  }
+}
