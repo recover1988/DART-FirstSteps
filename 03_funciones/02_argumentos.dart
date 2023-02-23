bab223247e24365aadebce2dc568df0661907986
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-23 01:16:54 -0300

    pasar arg por nombre

--- 03_funciones/02_argumentos.dart
+++ 03_funciones/02_argumentos.dart
@@ -3,6 +3,12 @@ void saludar(String mensaje,
   print('$mensaje $nombre - $edad');
 }
 
+void saludar2(String mensaje, {required String nombre, int veces = 10}) {
+  // cuerpo de la funcion
+  print('Saludar2: $mensaje $nombre - $veces');
+}
+
 void main(List<String> args) {
   saludar('Hola', 'Eric', 23);
+  saludar2('saludo', nombre: 'Toni');
 }
