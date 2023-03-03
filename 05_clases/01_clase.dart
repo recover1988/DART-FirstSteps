b06191226fed0f8bd1c967e377a9a79c5c9c08de
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-03-03 18:42:42 -0300

    constructores con nombre

--- 05_clases/01_clase.dart
+++ 05_clases/01_clase.dart
@@ -1,13 +1,14 @@
 import 'clases/persona.dart';
 
 void main(List<String> args) {
-  final persona = new Persona();
-  persona
-    ..nombre = 'Fernando'
-    ..edad = 23;
+  final persona = new Persona(edad: 33, nombre: 'fernando');
+  final persona2 = new Persona.persona30('Maria');
+  // persona
+  //   ..nombre = 'Fernando'
+  //   ..edad = 23;
   // ..bio = 'Guadalajara';
 
-  persona.bio = 'Cambie el valor';
+  // persona.bio = 'Cambie el valor';
 
   print(persona.info);
 }
